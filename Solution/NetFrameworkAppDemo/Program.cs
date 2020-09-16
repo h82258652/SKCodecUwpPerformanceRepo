740d679b25559d33a50afff851e4a14000fc2b1f
h82258652 <842053625@qq.com>
2020-09-16 21:31:51 +0800

    commit demo

---
+++ Solution/NetFrameworkAppDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using SkiaSharp;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace NetFrameworkAppDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var stream = File.OpenRead("test.png"))
+            {
+                using (var bitmap = new SKBitmap(960, 720))
+                {
+                    using (var canvas = new SKCanvas(bitmap))
+                    {
+                        using (var codec = SKCodec.Create(stream))
+                        {
+                            using (var decodeBitmap = new SKBitmap(codec.Info))
+                            {
+                                var pointer = decodeBitmap.GetPixels();
+                                codec.GetPixels(decodeBitmap.Info, pointer);
+
+                                var dest = new SKRect(0, 0, 960, 720);
+
+                                var stopwatch = new Stopwatch();
+                                stopwatch.Start();
+
+                                canvas.DrawBitmap(decodeBitmap, dest);
+
+                                stopwatch.Stop();
+
+                                Console.WriteLine($"cost {stopwatch.ElapsedMilliseconds} ms");
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Solution/UwpDemo/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using SkiaSharp;
+using System;
+using System.Diagnostics;
+using System.IO;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+
+namespace UwpDemo
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void RunButton_Click(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///test.png"));
+            using (var stream = await file.OpenStreamForReadAsync())
+            {
+                using (var bitmap = new SKBitmap(960, 720))
+                {
+                    using (var canvas = new SKCanvas(bitmap))
+                    {
+                        using (var codec = SKCodec.Create(stream))
+                        {
+                            using (var decodeBitmap = new SKBitmap(codec.Info))
+                            {
+                                var pointer = decodeBitmap.GetPixels();
+                                codec.GetPixels(decodeBitmap.Info, pointer);
+
+                                var dest = new SKRect(0, 0, 960, 720);
+
+                                var stopwatch = new Stopwatch();
+                                stopwatch.Start();
+
+                                canvas.DrawBitmap(decodeBitmap, dest);
+
+                                stopwatch.Stop();
+
+                                await new MessageDialog($"cost {stopwatch.ElapsedMilliseconds} ms").ShowAsync();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
